31415c68f90e1f69ffbea8edb64438ee9d744076
Mayank Gupta <you@example.com>
2023-07-30 00:27:33 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -7,66 +7,62 @@ const Gallery = require("../Model/galleryModel")
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Gallery.find().then((cards)=>{
-    res.render('show', {cards});
+router.get('/', function (req, res, next) {
+  Gallery.find().then((cards) => {
+    res.render('show', { cards });
   })
-  .catch((err)=> res.send(err))
+    .catch((err) => res.send(err))
 });
 
-
-router.get('/add', function(req, res, next) {
+router.get('/add', function (req, res, next) {
   res.render('add');
 });
 
-
-router.post('/add', function(req, res, next) {
-  upload (req,res, function (err){
+router.post('/add', function (req, res, next) {
+  upload(req, res, function (err) {
     if (err) return res.send(err);
-  Gallery.create({
-    title:req.body.title,
-    author:req.body.author,
-    image:req.file.filename,
-  }).then(()=> {
-    res.redirect("/")
-  })
-  .catch((err) => res.send(err))
+    Gallery.create({
+      title: req.body.title,
+      author: req.body.author,
+      image: req.file.filename,
+    }).then(() => {
+      res.redirect("/")
+    })
+      .catch((err) => res.send(err))
   })
 });
 
+router.get('/update/:id', function (req, res, next) {
+  Gallery.findById(req.params.id).then((card) => {
+    res.render("update", { card })
 
-router.get('/update/:id', function(req, res, next) {
-  Gallery.findById(req.params.id).then((card) =>{
-    res.render("update",{card})
-
-  }).catch((err) => res.send(err)) 
+  }).catch((err) => res.send(err))
 });
 
-router.get('/delete/:id', function(req, res, next) {
+router.get('/delete/:id', function (req, res, next) {
   const id = req.params.id
-  Gallery.findByIdAndDelete(req.params.id).then((deletedData) =>{
-    fs.unlinkSync(path.join(__dirname,"..","public","uploads",deletedData.image))
+  Gallery.findByIdAndDelete(req.params.id).then((deletedData) => {
+    fs.unlinkSync(path.join(__dirname, "..", "public", "uploads", deletedData.image))
     res.redirect("/")
   })
-  .catch((err) => res.send(err))
+    .catch((err) => res.send(err))
 });
 
-router.post('/update/:id', function(req, res, next) {
-  upload (req,res, function (err){
+router.post('/update/:id', function (req, res, next) {
+  upload(req, res, function (err) {
     if (err) return res.send(err);
-  const updateddata = {
-    title: req.body.title,
-    author: req.body.author,
-  }
-  if(req.file){
-    fs.unlinkSync(path.join(__dirname,"..","public","uploads",req.body.oldgallery));
-  updateddata.image = req.file.filename;
-  }
-  Gallery.findByIdAndUpdate(req.params.id, updateddata).then(()=>{
-    res.redirect("/")  
-  }).catch((err)=>res.send(err))
+    const updateddata = {
+      title: req.body.title,
+      author: req.body.author,
+    }
+    if (req.file) {
+      fs.unlinkSync(path.join(__dirname, "..", "public", "uploads", req.body.oldgallery));
+      updateddata.image = req.file.filename;
+    }
+    Gallery.findByIdAndUpdate(req.params.id, updateddata).then(() => {
+      res.redirect("/")
+    }).catch((err) => res.send(err))
+  });
 });
-});
-
 
 module.exports = router;
